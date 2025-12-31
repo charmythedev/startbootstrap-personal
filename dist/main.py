13e8ec7d2c101bfff3e98bc81e75c85a7584b917
Charmy <charmythedev@gmail.com>
2025-12-31 09:56:38 -0500

    personalize projects, resume
    used for loops to extracts .json data from api npoint for use in flask app. addedresume download option, fixed links for projects.

--- dist/main.py
+++ dist/main.py
@@ -1,18 +1,29 @@
 from datetime import datetime
 from flask import Flask, abort, render_template, redirect, url_for, flash, request
+import requests
 
+
+# App initialization
 current_year = datetime.now().year
 app = Flask(__name__)
 
 
+# Example project data
+response = requests.get("https://api.npoint.io/2d62bbc55683f8cfc99e")
+response.raise_for_status()
+projects = response.json()
+
+
+
+# ROUTES
 @app.route("/")
 def index():
     github_url = "https://github.com/charmythedev"
     return render_template("index.html", current_year=current_year, github_url=github_url)
 
 @app.route("/projects")
-def projects():
-    return render_template("projects.html", current_year=current_year)
+def show_projects():
+    return render_template("projects.html", current_year=current_year, projects =projects)
 
 @app.route("/contact", methods=["GET", "POST"])
 def contact():
